1dfd95d3e346554a7bba2459d137fde114878e2a
Reezky Ilma <rhhezkyillma@gmail.com>
2019-07-04 17:31:00 +0700

    daaADA

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.dashboard');
+});
+Route::get('/buku','Databuku@index')->name('buku.index');
+
+Route::get('/create', 'Databuku@create')->name('databuku.create');
+    
+Route::post('/save', 'DataBuku@store')->name('databuku.store');
+
+
+Route::get('/delete/{id}', 'DataBuku@destroy')->name('databuku.destroy');
+Route::get('/edit/{id}', 'DataBuku@edit')->name('databuku.edit');
+Route::post('/update/{id}', 'DataBuku@update')->name('databuku.update');
